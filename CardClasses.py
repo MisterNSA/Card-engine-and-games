066be26c306f7d273c3f8995c50aac32bf2b5a53
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-08-25 10:19:31 +0200

    Added "name" variable to Card class
    Also updatet the Setter and Getter.

--- CardClasses.py
+++ CardClasses.py
@@ -1,25 +1,49 @@
 #####################################################################################
 # Description: This is ment to be a Base for all diffrent kinds of Card game        #
 # Creator: Tobias Dominik Weber aka MisterNSA                                       #
-# Version: 0.1 24.07.2020                                                           #
+# Version: 0.2 25.08.2020                                                           #
 #####################################################################################
 
 import random
 
 
 class Card(object):
+    # ------------------------------------------------------------------- Builder ----------------------------------------------------------------
+
     # string - suit = Spades, Clubs, Diamond or Hearts
-    # string - value = 1 to 10, Jack, Queen, King, Ace
+    # string - value = from 2 to 14
+    # string - name = The full name of the cards - easier to read for Humans but useless for operations
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
+        self.set_name()
 
-    # Shows the Value in a easy to read way
+    # ------------------------------------------------------------------- Setter -----------------------------------------------------------------
+
+    # Builds name by value adn suit: 2 to 10, Jack, Queen, King, Ace
+    def set_name(self):
+        if self.value <= 10:
+            self.name = (str(self.value) + " of " + self.suit)
+        elif self.value == 11:
+            self.name = ("Jack of " + self.suit)
+        elif self.value == 12:
+            self.name = ("Queen of " + self.suit)
+        elif self.value == 13:
+            self.name = ("King of " + self.suit)
+        elif self.value == 14:
+            self.name = ("Ace of " + self.suit)
+
+    # ------------------------------------------------------------------- Getter ----------------------------------------------------------------- 
+
+    # returns the name of card
     def show(self):
-        return("{} of {}".format(self.value, self.suit))
+        return self.name
+
 
 
 class Deck(object):
+    # ------------------------------------------------------------------- Builder ----------------------------------------------------------------
+
     def __init__(self):
         # list - cards = The List of all cards stored in this deck
         self.cards = []
@@ -27,16 +51,13 @@ class Deck(object):
         self.build()
         self.shuffle()
 
+    # ------------------------------------------------------------------- Setter -----------------------------------------------------------------
+
     # initialise Cards
     def build(self):
-        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
-            for v in range(1, 14):
-                self.cards.append(Card(s, v))
-
-    # prints all cards in Deck
-    def show(self):
-        for c in self.cards:
-            print(c.show())
+        for suit in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for value in range(2, 15):
+                self.cards.append(Card(suit, value))
 
     # shuffles the cards
     def shuffle(self):
@@ -44,22 +65,40 @@ class Deck(object):
             r = random.randint(0, i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
 
+    # ------------------------------------------------------------------- Getter -----------------------------------------------------------------
+
     # returns a card and pops it from the Deck
     def drawCard(self):
         return self.cards.pop()
 
+    # prints all cards in Deck
+    def show(self):
+        for card in self.cards:
+            print(card.show())
+
+
 
 class Player(object):
+    # ------------------------------------------------------------------- Builder ----------------------------------------------------------------
+
     def __init__(self, name):
         # string - name = name of the Player | Questionable, if needed
         # list - hand = All Cards the Player has
         self.name = name
         self.hand = []
 
+    # ------------------------------------------------------------------- Setter -----------------------------------------------------------------
+
     # adds a card from the deck to the players hand
     def draw(self, Deck):
         self.hand.append(Deck.drawCard())
         return self
+
+    # takes the position of a card and removes it from the hand
+    def discard(self, placeInHand):
+        return self.hand.pop(placeInHand - 1)
+
+    # ------------------------------------------------------------------- Getter -----------------------------------------------------------------
 
     # prints the hand with the position of each card
     def showHand(self):
@@ -68,7 +107,4 @@ class Player(object):
             print("Card {} = {}".format(count, card.show()))
             count += 1
 
-    # takes the position of a card and removes it from the hand
-    def discard(self, placeInHand):
-        return self.hand.pop(placeInHand - 1)
 
