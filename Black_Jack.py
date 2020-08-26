7a1c9d101b73e2058d9c24f86bb1863ec2babb70
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-08-26 14:06:39 +0200

    Added the Game

--- Black_Jack.py
+++ Black_Jack.py
@@ -3,10 +3,23 @@
 # This game is build upon my CardClasses Library for Card games                     #
 # Creator: Tobias Dominik Weber aka MisterNSA                                       #
 # Version: 0.0 24.07.2020                                                           #
-# Planned Features: GUI with custom pixelart Cards,                                 #     
+# Planned Features: GUI with custom pixelart Cards,                                 #
 #####################################################################################
 
 from CardClasses import *
+from operator import attrgetter
+
+
+class Bj_Player(Player):
+    def __init__(self):
+        # string - name = name of the Player
+        # list - hand = All Cards the Player has
+        # bool - draw_again = Save when player doesnt wants to draw again
+        self.name = input("Please enter your name: ")
+        self.hand = []
+        self.draw_again = True
+        self.hand_value = 0
+
 
 class Blackjack():
     # ---------------------------------------------------------------------- Setup -----------------------------------------------------------------
@@ -19,35 +32,56 @@ class Blackjack():
         self.play()
 
     # ---------------------------------------------------------------------- Setter ----------------------------------------------------------------
-    
+
     # Creates the players
     def set_players(self):
         self.players = []
-        for player in range(0, int(input("How many Players are Playing? "))):
-            self.players.append(Player())
+        for _ in range(0, int(input("How many Players are Playing? "))):
+            self.players.append(Bj_Player())
 
     # ---------------------------------------------------------------------- Main Game -------------------------------------------------------------
 
     def play(self):
         game_running = True
+        # int - Players_done = Used to see, if all players are done drawing cards
+        Players_done = 0
+
+        # Game Loop
         while game_running:
-            for Player in self.players:
-                wants_to_draw = input(f"{Player.name}, do you want to draw another Card? ")
-                if wants_to_draw.lower() in ["y", "yes", "j", "ja"]:
-                    Player.draw(self.deck)
-                    Player.showHand()
+            while Players_done < len(self.players):
+                for Bj_Player in self.players:
+                    if Bj_Player.draw_again:
+                        wants_to_draw = input(
+                            f"{Bj_Player.name}, do you want to draw another Card? ")
+                        if wants_to_draw.lower() in ["y", "yes", "j", "ja"]:
+                            Bj_Player.draw(self.deck)
+                            Bj_Player.showHand()
+                            Bj_Player.set_hand_value()
+                        else:
+                            Bj_Player.draw_again = False
+                            Players_done += 1
+            else:
+                game_running = False
 
+        # Evaluate Winner #
+        # List - could_win = For Players that could have won. So you can keep all Players for the next round,
+        # but only need to check to check for Players with valid Score
+        could_win = []
+        for Bj_Player in self.players:
+            if Bj_Player.hand_value < 21:
+                could_win.append(Bj_Player)
 
-        # Spieler zieht eine Karte
-        # Nächster Spieler ist dran, Loop wiederholen
-
-        # Wenn alle 3 Spieler nicht mehr ziehen wollen, wird ausgewertet
-        # Kartenwerte addieren und prüfen, ob sie >= 21 sind
-        # Verbleibende Spieler miteinander vergleichen und größte zahl auswählen
-        # Spieler mit dieser Höchsten Zahl gewinnt
-
-
-
+        # Check who won, or if the game was a draw
+        if len(could_win) > 0:
+            could_win.sort(key=attrgetter("hand_value"), reverse=True)
+            # I could print out between which players the draw occured, but for now im to lazy
+            if len(could_win) > 1:
+                if could_win[0].hand_value == could_win[1].hand_value:
+                    print(f"Draw!")
+            else:
+                print(f"{could_win[0].name} has Won!")
+        else:
+            print("Draw, neither of you got under the Score of 21!")
 
 
 game = Blackjack()
