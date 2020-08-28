b20b8f84b8166aab71fed03fc5d1fabc130acf09
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-08-28 14:49:26 +0200

    Updated Description, added Exceptions

--- Black_Jack.py
+++ Black_Jack.py
@@ -22,6 +22,7 @@ class Bj_Player(Player):
 
 
 class Blackjack():
+    """ This Class represents the Game of Blackjack"""
     # ---------------------------------------------------------------------- Setup -----------------------------------------------------------------
 
     def __init__(self):
@@ -31,22 +32,38 @@ class Blackjack():
 
         self.play()
 
-    # ---------------------------------------------------------------------- Setter ----------------------------------------------------------------
-
-    # Creates the players
+    
     def set_players(self):
+        """Ask how many Players are playing and creates them"""
         self.players = []
-        for _ in range(0, int(input("How many Players are Playing? "))):
+
+        valid = False
+        while valid = False:
+            try:
+                player_number = int(input("How many Players are Playing? "))
+                valid = True
+            except:
+                print("Das war keine valide Eingabe. Bitte gib eine Zahl ein.")
+        # create the given Number of Players
+        for _ in range(0, player_number):
             self.players.append(Bj_Player())
 
     # ---------------------------------------------------------------------- Main Game -------------------------------------------------------------
 
     def play(self):
+        """
+        The method to play the game
+        Lets the Players draw cards as long as they want.
+        If no one wants to draw again, the method evaluates who has lost because of a Handvalue over 21
+        Then it evaluates which Player has the Highest Score and returns the Winner, or a draw if more than one Person has the best Score 
+        """
+        # bool - game_nunning = Flag to evaluate if the game is still running, or if the winner should be evaluated
         game_running = True
         # int - Players_done = Used to see, if all players are done drawing cards
         Players_done = 0
-
-        # Game Loop
+    # ---------------------------------------------------------------------- Game Loop -------------------------------------------------------------
+    
+        # Ask Players if they want to draw again, until no one wants to draw again
         while game_running:
             while Players_done < len(self.players):
                 for Bj_Player in self.players:
@@ -63,27 +80,31 @@ class Blackjack():
             else:
                 game_running = False
 
-        # Evaluate Winner #
+    # ---------------------------------------------------------------------- Evaluation ------------------------------------------------------------
+
         # List - could_win = For Players that could have won. So you can keep all Players for the next round,
         # but only need to check to check for Players with valid Score
         could_win = []
         for Bj_Player in self.players:
-            if Bj_Player.hand_value < 21:
+            if Bj_Player.hand_value <= 21:
                 could_win.append(Bj_Player)
 
         # Check who won, or if the game was a draw
+        # sort list by highest value of handcards
         if len(could_win) > 0:
             could_win.sort(key=attrgetter("hand_value"), reverse=True)
-            # I could print out between which players the draw occured, but for now im to lazy
+            ### I could print out between which players the draw occured, but for now im to lazy
             if len(could_win) > 1:
                 if could_win[0].hand_value == could_win[1].hand_value:
                     print(f"Draw!")
             else:
                 print(f"{could_win[0].name} has Won!")
         else:
-            print("Draw, neither of you got under the Score of 21!")
+            print("Draw, neither of you got under the Score of 22!")
 
 
 game = Blackjack()
 
-# Later add Lives, so that the game can be played as a best out of x
+# To Do
+# Fix Card Values
+# Add a exception if all cards were drawn from the deck
