f948a1c51ce5ec4ae491bb40c4a89dc46572b2c0
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-08-30 16:26:04 +0200

    Added Mechanics to Manipulate value
    Added self.symbol which represents the special Symbols from Jack to Ace.
    Added set_value to change the Cards Value.
    
    This is used to change the Card Values to match the games card values

--- Card.py
+++ Card.py
@@ -10,10 +10,12 @@ class Card(object):
 
     # string - suit = Spades, Clubs, Diamond or Hearts
     # string - value = from 2 to 14
+    # string - symbol = Holds special symbol from Jack to Ace, else None
     # string - name = The full name of the cards - easier to read for Humans but useless for operations
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
+        self.symbol = None
         self.set_name()
 
     # ------------------------------------------------------------------- Setup ------------------------------------------------------------------
@@ -24,15 +26,23 @@ class Card(object):
             self.name = (str(self.value) + " of " + self.suit)
         elif self.value == 11:
             self.name = ("Jack of " + self.suit)
+            self.symbol = "Jack"
         elif self.value == 12:
             self.name = ("Queen of " + self.suit)
+            self.symbol = "Queen"
         elif self.value == 13:
             self.name = ("King of " + self.suit)
+            self.symbol = "King"
         elif self.value == 14:
             self.name = ("Ace of " + self.suit)
+            self.symbol = "Ace"
 
     # ------------------------------------------------------------------- Misc -------------------------------------------------------------------
 
     def show(self):
         """returns the cards name"""
         return self.name
+
+    def set_value(self, value):
+        """Changes the Cards Value. Needed to game specific values"""
+        self.value = value
